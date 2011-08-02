a0cc36be2c02e0ca3822e9a582c85e553a3a4a0e
Gregory Mostizky <gregory.mostizky@gmail.com>
2011-08-02 12:01:08 +0300

    more bling

--- app/controllers/design_controller.rb
+++ app/controllers/design_controller.rb
@@ -1,7 +1,15 @@
+require 'ostruct'
+
 class DesignController < ApplicationController
 
   def index
+    layer_source = view_context.lookup_context.find('_layer_text','design').source # hack hack hack
+    all_params = layer_source.scan(/@params\[['"](.*)['"]\]/).flatten # hack hack 2
 
+    @params = {}
+    all_params.each do |k|
+      @params[k] = params[k] || ''
+    end
   end
 
 end
