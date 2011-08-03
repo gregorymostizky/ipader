ff2bb2e711960e57fcdc3fdca082bc3dcf5ac1aa
Gregory Mostizky <gregory.mostizky@gmail.com>
2011-08-03 22:44:05 +0300

    html5 t&l

--- app/controllers/design_controller.rb
+++ app/controllers/design_controller.rb
@@ -5,9 +5,11 @@ class DesignController < ApplicationController
   def index
     @layer_name = (params[:layer] || 'text')
     layer_source = view_context.lookup_context.find("_layer_"+@layer_name,'design').source # hack hack hack
-    all_params = layer_source.scan(/@params\[['"](.*)['"]\]/).flatten # hack hack 2
+    all_params = layer_source.scan(/@params\[['"](\w+)['"]\]/).flatten # hack hack 2
 
     @params = {}
+    p all_params
+
     all_params.each do |k|
       unless params[k].blank?
         @params[k] = params[k]
