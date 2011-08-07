da3698870515b7136d01b23813acd02ccdee92d3
Gregory Mostizky <gregory.mostizky@gmail.com>
2011-08-07 18:09:52 +0300

    some more layout

--- public/farbtastic/farbtastic.js
+++ public/farbtastic/farbtastic.js
@@ -241,7 +241,7 @@ jQuery._farbtastic = function (container, callback) {
       // Set background/foreground color
       $(fb.callback).css({
         backgroundColor: fb.color,
-        color: fb.hsl[2] > 0.5 ? '#000' : '#fff'
+//        color: fb.hsl[2] > 0.5 ? '#000' : '#fff'
       });
 
       // Change linked value
